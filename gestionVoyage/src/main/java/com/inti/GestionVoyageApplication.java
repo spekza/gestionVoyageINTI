ca6c7be8daada2802e6e82cb03c3e916d0517aac
spekza <74365510+spekza@users.noreply.github.com>
2023-05-22 15:33:16 +0200

    Add files via upload

---
+++ gestionVoyage/src/main/java/com/inti/GestionVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVoyageApplication.class, args);
+	}
+
+}
