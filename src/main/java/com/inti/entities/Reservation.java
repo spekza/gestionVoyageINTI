d71034d3fcd0d7cd1a645a86868d6dfd6fb26e85
spekza <vincent.auconie26@gmail.com>
2023-05-23 10:14:33 +0200

    Add Reservation

---
+++ src/main/java/com/inti/entities/Reservation.java
@@ -0,0 +1,81 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RESERVATIONS", schema = "gestion_voyage_db")
+public class Reservation implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idReservation;
+	
+	private Date dateReservation;
+	private int nbJours;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_hotel")
+	private Hotel hotel;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_hotel")
+	private Voyageur voyageur;
+
+	public Long getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+	public int getNbJours() {
+		return nbJours;
+	}
+
+	public void setNbJours(int nbJours) {
+		this.nbJours = nbJours;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public Voyageur getVoyageur() {
+		return voyageur;
+	}
+
+	public void setVoyageur(Voyageur voyageur) {
+		this.voyageur = voyageur;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [idReservation=" + idReservation + ", dateReservation=" + dateReservation + ", nbJours="
+				+ nbJours + "]";
+	}
+
+	
+	
+	
+}
